a1db97ea29c27f5d3894a47d14a4083ee6515102
vaf <newspecem@gmail.com>
2022-02-02 11:56:34 -0300

    connect

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ const app = express()
 app.get("/", (req,res) => {
     res.write({object: null})
 })
-
+app.post("/send", (req,res) => {
+    const {name,temp,altitude,latitude,lagitude,speed,course} = req.body
+    
+})
 app.listen(8080)
